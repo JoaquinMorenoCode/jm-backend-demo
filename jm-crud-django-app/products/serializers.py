6e1c8375368c8beee3be97b2af44bafb522da0ab
Joaquin Moreno <joaquinmorenovfx@gmail.com>
2024-10-04 08:21:13 -0500

    clean class

--- jm-crud-django-app/products/serializers.py
+++ jm-crud-django-app/products/serializers.py
@@ -1,36 +1,6 @@
 from rest_framework import serializers
 from products.models import Product  
 
-# class ProductSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(required=True, allow_blank=False, max_length=100)
-#     description = serializers.CharField(required=False, allow_blank=True, max_length=300)
-#     price = serializers.DecimalField(required=True,min_value=0.01)
-#     stock = serializers.IntegerField(required=True,min=0)
-#     created_at = serializers.DateTimeField()
-#     updated_at = serializers.DateTimeField()
-
-
-#     def create(self, validated_data):
-#         """
-#         Create and return a new `Snippet` instance, given the validated data.
-#         """
-#         return Product.objects.create(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         """
-#         Update and return an existing `Snippet` instance, given the validated data.
-#         """
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.description = validated_data.get('description', instance.description)
-#         instance.price = validated_data.get('price', instance.price)
-#         instance.stock = validated_data.get('stock', instance.stock)
-#         instance.created_at = validated_data.get('created_at', instance.created_at)
-#         instance.updated_at = validated_data.get('updated_at', instance.updated_at)
-
-#         instance.save()
-#         return instance
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
